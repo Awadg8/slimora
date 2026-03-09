380bd8e816c1a636c8d6139dc010e624182622b1
Awadhesh Gupta <awadheshg798@gmail.com>
2026-03-09 14:41:23 +0530

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { Footer } from "@/component/Footer";
+import MentalLoad from "@/section/home/mentalLoad";
+
 export default function Home() {
-  return <main>
+  return (
+    <main>
+      <MentalLoad />
     <Footer/>
-  </main>;
+    </main>
+  );
 }
