ba964a872d4509d9dbc62988d1c8d9bb6436ea38
neelam177 <gneelam2004@gmail.com>
2026-03-10 17:06:44 +0530

    commit

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -1,10 +1,13 @@
 import Header from "@/component/header";
 import Footer from "@/component/footer";
-
+import HowItWork from "@/section/howItWork/howItWork";
+import HowItHelps from "@/section/howItWork/howItHelps";
 const page = () => {
   return (
     <section>
       <Header />
+      <HowItWork/>
+      <HowItHelps/>
       <Footer />
     </section>
   );
