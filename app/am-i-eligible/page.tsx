31a9bb4d06aa9387a5d152930d8a65b5fb0e2ca4
HarshJain-01 <microlifessciences@gmail.com>
2026-03-11 10:00:34 +0530

    Add Others section and adjust banner styles
    Introduce a new Others component with eligibility copy and a 4-item grid, and include it (along with CheckIf) on the AmEligible page. Update banner styles for better mobile responsiveness: reduce mobile banner height from 80vh to 50vh and adjust typography/line-height for the banner headings/subheadings.

--- app/am-i-eligible/page.tsx
+++ app/am-i-eligible/page.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import Header from '@/component/header';
 import Footer from '@/component/footer';
 import Banner from '@/section/am-i-eligible/banner';
+import CheckIf from '@/section/getStarted/checkIf';
+import Others from '@/section/am-i-eligible/others';
+import GastricBalloon from '@/section/getStarted/gastricBalloon'
+
 export default function AmEligible(){
   return (
     <div className="max-w-[2560px] mx-auto">
       <Header/>
       <Banner/>
+      <Others/>
+      <CheckIf/>
+      <GastricBalloon/>
       <Footer/>
     </div>
   )
--- section/am-i-eligible/banner.tsx
+++ section/am-i-eligible/banner.tsx
@@ -12,7 +12,7 @@ export default function Banner(){
         />
         <div className="hidden min-[1025px]:block absolute bottom-0 left-0 right-0 h-32 bg-linear-to-t from-[#0A1D20] via-[#0a1d2065] to-transparent" />
       </div>
-      <div className="block sm:hidden relative w-full h-[80vh]">
+      <div className="block sm:hidden relative w-full h-[50vh]">
         <Image
           src={"/images/am_i_eligible/mobile_banner.png"}
           fill
@@ -21,9 +21,9 @@ export default function Banner(){
         />
       </div>
       <div className="absolute bottom-10 min-[1025px]:relative flex flex-col items-center min-[1025px]:items-start min-[1025px]:mt-0 w-full min-[1025px]:w-[40%]">
-        <p className="text-[#b5b5b5] font-urbanist text-[20px] sm:text-[26px]">Start Your Journey</p>
-        <p className="text-[#8EA94D] font-urbanist text-[48px] sm:text-[64px] font-semibold">BMI Calculator</p>
-        <p className="text-[#b5b5b5] font-urbanist text-[20px] sm:text-[26px]">Swallowable gastric balloon.</p>
+        <p className="text-[#b5b5b5] font-urbanist text-[12px] sm:leading-[30px] sm:text-[26px]">Start Your Journey</p>
+        <p className="text-[#8EA94D] font-urbanist text-[24px] sm:leading-[68px] sm:text-[64px] font-semibold">BMI Calculator</p>
+        <p className="text-[#b5b5b5] font-urbanist text-[12px] sm:leading-[30px] sm:text-[26px]">Swallowable gastric balloon.</p>
       </div>
     </div>
   )
