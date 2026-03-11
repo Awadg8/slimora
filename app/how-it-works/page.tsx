d64f56ba786157155b690d7a408f5799db9789cd
neelam177 <gneelam2004@gmail.com>
2026-03-11 14:52:26 +0530

    commit

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -2,12 +2,21 @@ import Header from "@/component/header";
 import Footer from "@/component/footer";
 import HowItWork from "@/section/howItWork/howItWork";
 import HowItHelps from "@/section/howItWork/howItHelps";
+import HowBalloonPlaced from "@/section/howItWork/howBalloonPlaced";
+import BalloonRemovalAndEligibility from "@/section/howItWork/balloonRemovalAndEligibility";
+import MoreThanBalloon from "@/section/howItWork/moreThanBalloon";
+import ReadyToTake from "@/component/readyToTake";
+
 const page = () => {
   return (
     <section>
       <Header />
-      <HowItWork/>
-      <HowItHelps/>
+      <HowItWork />
+      <HowItHelps />
+      <HowBalloonPlaced />
+      <BalloonRemovalAndEligibility />
+      <MoreThanBalloon />
+      <ReadyToTake/>
       <Footer />
     </section>
   );
--- section/howItWork/howItHelps.tsx
+++ section/howItWork/howItHelps.tsx
@@ -19,30 +19,32 @@ export default function HowItHelps() {
     return (
         <div className='bg-[#0A1D20]'>
             <div className='max-width pb-[100px] md:pb-[200px]'>
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-[33px] md:gap-[125px]'>
-                    <div className='relative h-[247px] md:h-[344px] w-full md:w-[608px] overflow-hidden'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-[33px] lg:gap-[125px] md:gap-[80px]'>
+                    <div className='relative h-[247px] lg:h-[344px] md:h-[330px] w-full  overflow-hidden'>
                         <Image
                             src="/images/how_it_works/weight_loss2.png"
                             alt='Weight'
-                            width={352}
-                            height={247}
-                            className='w-full h-auto object-cover md:hidden'
+                            fill
+                            className='w-full h-auto object-cover block lg:hidden'
                         />
                         <Image
                             src="/images/how_it_works/weight_loss.png"
                             alt='Weight'
-                            width={608}
-                            height={344}
-                            className='w-full h-auto object-cover hidden md:block'
+                            fill
+                            className='w-full h-auto object-cover hidden lg:block'
                         />
+                        {/* Gradient overlay only on lg */}
+                         <div className="hidden lg:block absolute inset-0 
+  bg-[linear-gradient(270deg,#0A1D20_0%,rgba(10,29,32,0)_100%)]"></div>
+
                     </div>
                     <div className='flex justify-between items-center'>
                         <p className='text-[#FFFFFF] text-[22px] md:text-[35px] font-bold text-center md:text-left'>How does SlimOra Program Balloon help with weight loss?</p>
                     </div>
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-3 pt-[69px] gap-[21px]'>
+                <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 pt-[69px] gap-[21px]'>
                     {helpCards.map((card, index) => (
-                        <div key={index} className="rounded-[20px] opacity-[0.5px] bg-[linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2)),linear-gradient(180deg,#143338_0%,#2D4C3D_23.56%,#8EA94D_100%)] md:h-[244px] py-[30px] md:py-[40px] px-[10px] justify-center flex flex-col">
+                        <div key={index} className="rounded-[20px] opacity-[0.5px] bg-[linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2)),linear-gradient(180deg,#143338_0%,#2D4C3D_23.56%,#8EA94D_100%)] py-[30px] md:py-[40px] px-[10px] justify-center flex flex-col">
                             <div className='gap-[20px] md:gap-[28px] flex flex-col'>
                                 <h1 className='text-[#FFFFFF] text-center text-[24px] font-semibold'>{card.title}</h1>
                                 <p className='text-[12px] md:text-[18px] text-[#B5B5B5] leading-[18.271px] md:leading-[24px] text-center'>{card.description}</p>
