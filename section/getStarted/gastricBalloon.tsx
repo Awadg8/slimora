c8b406665882cebcb64bdd54bf18b588b99569a5
aftabaj <aftabakoct172326@gmail.com>
2026-03-11 16:17:41 +0530

    Update gastricBalloon.tsx

--- section/getStarted/gastricBalloon.tsx
+++ section/getStarted/gastricBalloon.tsx
@@ -4,7 +4,7 @@ import CtaButton from '@/component/ctaButton'
 
 export default function GastricBalloon() {
     return (
-        <div className='pb-30 pt-15 sm:pt-30 sm:pb-30 md:pt-50 md:pb-50 relative overflow-hidden '>
+        <div className='pt-30 pb-30 md:pt-50 md:pb-50 relative overflow-hidden '>
             <div
                 className='hidden md:block absolute -left-50  z-0 '
                 style={{
@@ -25,7 +25,7 @@ export default function GastricBalloon() {
                 }}
             >
             </div>
-            <div className='max-width flex flex-col md:flex-row items-center justify-between gap-8'>
+            <div className='max-width flex flex-col md:flex-row items-center justify-between gap-8 '>
 
                 {/* Left Section - Text  */}
                 <div className=' md:w-[25%] z-1'>
@@ -35,7 +35,7 @@ export default function GastricBalloon() {
                 </div>
 
                 {/* Center Section - Image with Button */}
-                <div className='relative md:w-[50%] space-y-8 sm:space-y-0'>
+                <div className='relative md:w-[50%]'>
                     <div className='relative rounded-[22px] md:rounded-[44px] overflow-hidden'>
                         <Image
                             src='/images/get_started/learn_more.jpg'
@@ -46,18 +46,18 @@ export default function GastricBalloon() {
                         />
                         {/* Learn More Button */}
                         <div className='absolute inset-0 flex items-center justify-center'>
-                            <CtaButton text='Learn More' type='link' href='#' className='w-[145px] sm:w-62.5 tex-[12px] sm:text-[18px] text-[#b5b5b5] font-medium' />
+                            <CtaButton text='Learn More' type='link' href='#' className='w-62.5 text-[18px] font-medium' />
                         </div>
                     </div>
+                </div>
 
-                    {/* Right Section - Text */}
-                    <div className='md:w-[25%] z-1'>
-                        <p className='text-[12px] md:text-[18px] text-center text-[#B5B5B5]'>
-                            To learn more about the <span className='font-bold'>SlimOra Gastric Balloon,</span> assess the risks and benefits of this solution, read the important safety information, and discuss it with your doctor.
-                        </p>
-                    </div>
+                {/* Right Section - Text */}
+                <div className='md:w-[25%] z-1'>
+                    <p className='text-[12px] md:text-[18px] text-center text-[#B5B5B5]'>
+                        To learn more about the <span className='font-bold'>SlimOra Gastric Balloon,</span> assess the risks and benefits of this solution, read the important safety information, and discuss it with your doctor.
+                    </p>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
